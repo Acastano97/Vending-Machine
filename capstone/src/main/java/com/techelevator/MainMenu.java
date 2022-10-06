9b6bb90f481b57ce010e814aa58aea55ede95672
Henry Fossett <henry.fossett@gmail.com>
2022-10-06 09:58:18 -0400

    first commit

---
+++ capstone/src/main/java/com/techelevator/MainMenu.java
@@ -0,0 +1,17 @@
+package com.techelevator;
+
+public class MainMenu extends Menu{
+
+
+    public MainMenu(String firstSlot, String secondSlot, String thirdSlot) {
+        super(firstSlot, secondSlot, thirdSlot);
+        firstSlot = "Display Vending Machine Items";
+        secondSlot = "Purchase";
+        thirdSlot = "Exit";
+
+
+    }
+
+
+}
+
---
+++ capstone/src/main/java/com/techelevator/Menu.java
@@ -0,0 +1,29 @@
+package com.techelevator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Menu {
+    public static void main(String[] args) {
+
+    }
+
+    public Menu(String firstSlot, String secondSlot, String thirdSlot) {
+
+        List<String> slots = new ArrayList<>();
+        slots.add(firstSlot);
+        slots.add(secondSlot);
+        slots.add(thirdSlot);
+
+        for(int i = 0; i < slots.size(); i++){
+            System.out.println("("+(i+1)+") " + slots.indexOf(i+1));
+        }
+
+
+        System.out.printf("(1) %s \n (2) %s \n, (3) %s \n", firstSlot, secondSlot, thirdSlot);
+    }
+
+
+
+
+}
